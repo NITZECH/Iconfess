01807680f1b0587a9127e5ecbafb3fc3a7decfe5
Nitesh_Devkota <niteshdevkota2016@gmail.com>
2021-05-22 12:53:14 +0545

    simple architecture

---
+++ client/src/Components/demo.js
@@ -0,0 +1,12 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+
+export default function Demo()  {
+    return(
+        <Grid container spacing={3}>
+        <Grid alignItems="center" textAlign="center" item xs={10}>
+          <h1>hello</h1>
+
+        </Grid>
+      </Grid>
+    )}
